f2320d1e6cf655304b1ea1a53457b52d9cb68042
Mohammadxoja2005 <rustamxojayev.muhammadxoja@yandex.ru>
2023-10-14 15:25:44 +0500

    added index.d.ts

---
+++ src/index.d.ts
@@ -0,0 +1,11 @@
+import { ReactNode } from "react";
+
+// declare module 'fetcher-lala/components/FetchLoader' {
+    export interface IFetchLoaderProps<T> {
+        Loader: () => ReactNode;
+        children: (p: any) => Promise<ReactNode> | Promise<void>;
+        payload?: T;
+        deps?: any[];
+        errorText?: string
+    }
+// }
