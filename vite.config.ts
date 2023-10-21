1d53dcc66b91141fa01c74fc472cdec699c11eba
Mohammadxoja2005 <rustamxojayev.muhammadxoja@yandex.ru>
2023-10-21 16:44:03 +0500

    added externals

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig({ 
+  plugins: [react()], 
+  build: {
+
+    /** If you set esmExternals to true, this plugins assumes that 
+      all external dependencies are ES modules */
+ 
+    commonjsOptions: {
+       esmExternals: true 
+    },
+ }
 })
